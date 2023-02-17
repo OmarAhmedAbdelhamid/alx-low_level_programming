c0b451c1fa3bc1260ef184b74abb7b7b648569ad
OmarAhmedAbdelhamid <spectreomar242003@gmail.com>
2023-02-17 14:30:29 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ *  main - Entry point
+ *  Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+		       putchar(c);
+		       c++;
+		}
+	putchar('\n');	
+	return (0);
+}
